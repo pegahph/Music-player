c510e5241bc69641cf390a6f455fdf8043735b9b
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2020-12-28 00:33:20 +0330

    Artists sorted by name.

--- app/src/main/java/com/example/musicplayer/ArtistsFragment.java
+++ app/src/main/java/com/example/musicplayer/ArtistsFragment.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ArtistsFragment extends Fragment {
 
@@ -20,11 +22,21 @@ public class ArtistsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_artists, container, false);
         artistsList = ListMaker.loadArtists();
+        sortByName();
         RecyclerView artistRV = (RecyclerView) view.findViewById(R.id.artists_recycler_view);
         ArtistAdapter artistAdapter = new ArtistAdapter(artistsList);
         artistRV.setAdapter(artistAdapter);
         artistRV.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
+    }
+
+    private void sortByName() {
+        Collections.sort(artistsList, new Comparator<Artist>() {
+            @Override
+            public int compare(Artist o1, Artist o2) {
+                return o1.getArtistName().compareTo(o2.getArtistName());
+            }
+        });
     }
 }
