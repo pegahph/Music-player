110e012100dd979c5dc39bc8cdea60cb6ba61313
Ghazaleh99 <Ghazaleh.hamzeh@gmail.com>
2020-12-31 21:17:23 +0330

    appWidget Java getting started.

--- app/src/main/java/com/example/musicplayer/MusicPlayerAppWidget.java
+++ app/src/main/java/com/example/musicplayer/MusicPlayerAppWidget.java
@@ -1,9 +1,12 @@
 package com.example.musicplayer;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Implementation of App Widget functionality.
@@ -28,6 +31,11 @@ public class MusicPlayerAppWidget extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+            Intent intent = new Intent(context, SongPlayerPage.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+            RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.music_player_app_widget);
+            views.setOnClickPendingIntent(R.id.imageView,pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
@@ -36,6 +44,7 @@ public class MusicPlayerAppWidget extends AppWidgetProvider {
         // When the user deletes the widget, delete the preference associated with it.
         for (int appWidgetId : appWidgetIds) {
             MusicPlayerAppWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
+            Toast.makeText(context,"deleted",Toast.LENGTH_SHORT).show();
         }
     }
 
