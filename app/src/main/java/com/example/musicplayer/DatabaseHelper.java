9e11b22d214a5e924e539a98626f0bb0c9e2f787
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-01-26 02:56:08 +0330

    Database works.

--- app/src/main/java/com/example/musicplayer/DatabaseHelper.java
+++ app/src/main/java/com/example/musicplayer/DatabaseHelper.java
@@ -14,12 +14,12 @@ import java.util.ArrayList;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
-    private static final String TAG = "DatabaseHelper";
     private static final String DATABASE_NAME = "playlistsDB";
     private static final String TABLE_NAME = "playlists";
-    private static final String KEY_ID = "id";
     private static final String KEY_NAME = "PlaylistTitle";
-    private static final String[] COLUMNS = {KEY_ID, KEY_NAME};
+    private static final String KEY_SONGS = "PlaylistSongs";
+    private static final String[] COLUMNS = {KEY_NAME, KEY_SONGS};
+    ArrayList<String> playlistTitles;
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,7 +27,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " ( " + KEY_NAME + " TEXT PRIMARY KEY )";
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " ( " + KEY_NAME + " TEXT PRIMARY KEY, " + KEY_SONGS + " TEXT )";
         db.execSQL(CREATE_TABLE);
     }
 
@@ -43,16 +43,38 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.close();
     }
 
-    public ArrayList<String> allPlaylistTitles() {
-        ArrayList<String> playlistTitles = new ArrayList<>();
+    public String getThisPlaylistSongs(String playlistName) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAME, // a. table
+                COLUMNS, // b. column names
+                " " + KEY_NAME +" = ?", // c. selections
+                new String[] { playlistName }, // d. selections args
+                null, // e. group by
+                null, // f. having
+                null, // g. order by
+                null); // h. limit
+
+        String playlistSongs = "";
+        if (cursor != null) {
+            cursor.moveToFirst();
+            playlistSongs = cursor.getString(cursor.getColumnIndex(KEY_SONGS));
+        }
+        return playlistSongs;
+    }
+
+
+    public ArrayList<String> getAllPlaylistTitles() {
+        if (playlistTitles != null)
+            return playlistTitles;
+        playlistTitles = new ArrayList<>();
         String query = "SELECT  * FROM " + TABLE_NAME;
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(query, null);
 
         if (cursor.moveToFirst()) {
             do {
-                String title = cursor.getString(cursor.getColumnIndex(KEY_NAME));
-                playlistTitles.add(title);
+                String playlistTitle = cursor.getString(cursor.getColumnIndex(KEY_NAME));
+                playlistTitles.add(playlistTitle);
             } while (cursor.moveToNext());
         }
 
@@ -60,16 +82,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return playlistTitles;
     }
 
-    public void addPlaylistTitle(String playlistTitle) {
+    public void addPlaylist(String playlistTitle, String playlistSongs) {
+        if (playlistTitles.contains(playlistTitle))
+        {
+            updatePlaylist(playlistTitle, playlistSongs);
+            return;
+        }
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(KEY_NAME, playlistTitle);
+        values.put(KEY_SONGS, playlistSongs);
         // insert
         db.insert(TABLE_NAME, null, values);
         db.close();
     }
 
-    public void updatePlaylistTitle() {
+    public int updatePlaylist(String playlistTitle, String playlistSongs) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, playlistTitle);
+        values.put(KEY_SONGS, playlistSongs);
 
+        int i = db.update(TABLE_NAME, // table
+                values, // column/value
+                KEY_NAME +" = ?", // selections
+                new String[] { playlistTitle });
+
+        db.close();
+
+        return i;
     }
 }
